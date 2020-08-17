cb3ee3e47ecd430970cc97c41ce2e7ea2eb0c84c
2e12 <gabrielbeutler@gmail.com>
2020-08-17 22:12:38 +0200

    Add remove object method

--- engine/scene/scene.py
+++ engine/scene/scene.py
@@ -24,6 +24,10 @@ class Scene:
     def add_object(self, obj):
         self.objects.append(obj)
 
+    def remove_object(self, obj):
+        if obj in self.objects:
+            self.objects.remove(obj)
+
     def set_player(self, obj):
         if obj not in self.objects:
             self.objects.append(obj)
